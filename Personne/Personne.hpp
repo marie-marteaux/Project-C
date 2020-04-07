81c1d7fa677350b7dc9ce2589951193a8acd160e
Yasmine <maazouz.yasmine.10@gmail.com>
2020-04-07 11:23:43 +0200

    Yasmine 7/04

---
+++ Personne/Personne.hpp
@@ -0,0 +1,26 @@
+#ifndef PERSONNE_H 
+#define PERSONNE_H 
+
+/*!
+*\file Personne.hpp
+*\brief {Définition d'une personne par ses caractéristiques}
+*\author Groupe A4
+*\version 0.1
+*\date 7 avril 2020
+
+\class Personne
+*/
+
+class Personne {   
+	private:         
+		str nom;
+		str prenom;
+		int age;
+		str adresse;
+		str mail;
+		int annee;
+		str departement;
+		
+}; 
+
+#endif // PERSONNE_H
