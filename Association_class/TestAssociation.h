8ec4e3f576f863dc9298fdce6e9c63bc190d5399
GuenoleF <guenole.fiche@gmail.com>
2020-04-13 14:02:16 +0200

    GuenoleF 13/04

---
+++ Association_class/TestAssociation.h
@@ -0,0 +1,29 @@
+#ifndef PROJECT_C_TESTASSOCIATION_H
+#define PROJECT_C_TESTASSOCIATION_H
+
+/*!
+ * \file TestAssociation.h
+ * \brief Test d'une association
+ * \author Groupe A4
+ * \version 0.1
+ */
+
+#include <vector>
+#include "Personne.hpp"
+#include "Association.hpp"
+
+/*!
+ * \Class TestAssociation
+ * \brief Cette classe teste une association
+ * Elle vérifie le bon fonctionnement des méthodes (à l'exception des accesseurs)
+ */
+
+class TestAssociation {
+
+};
+
+
+
+
+
+#endif
