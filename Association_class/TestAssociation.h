c18d3f623bdbe586745cfc355e370588fa639a37
guenoleF <guenole.fiche@gmail.com>
2020-04-19 23:30:09 +0200

    Guenole 19/04 Version avec test CppUnit

--- Association_class/TestAssociation.h
+++ Association_class/TestAssociation.h
@@ -1,16 +1,6 @@
 #ifndef TEST_ASSOCIATION_H
 #define TEST_ASSOCIATION_H
 
-/*
-#include "cppunit/TestCase.h"
-#include "cppunit/TestFixture.h"
-#include "cppunit/TestCaller.h"
-#include "cppunit/TestResult.h"
-#include "cppunit/TestSuite.h"
-#include "cppunit/CompilerOutputter.h"
-#include "cppunit/XmlOutputter.h"
-#include "cppunit/ui/text/TestRunner.h"
-*/
 
 #include <string>
 #include <vector>
@@ -20,18 +10,8 @@
 
 using namespace std;
 
-// Use those macros and repeat the class name three times in
-// CLASS_NAME, CLASS_NAME_STRING
-#define CLASS_NAME Association
 #define CLASS_NAME_STRING "Association"
-#define OUTPUT_XML_FILE "output.xml"
 
-/*
-#define TEST_DECL(x) void test_##x()
-#define TEST_ADD(name) \
-	suite->addTest(new CppUnit::TestCaller<TestAssociation>("test_"#name, \
-		&TestAssociation::test_##name));
-*/
 
 class TestAssociation : public CppUnit::TestFixture {
 private:
